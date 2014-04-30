53ff2edb871dfec39249a2151a3cb8c058e0b434
foxox <foxox.jd@live.com>
2014-04-30 13:23:11 -0400

    Added LD sampler example projects

---
+++ samplerexamples/exhaltonscrambledsampler2dmain.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+#include "PixelSampleExampleGenerators.h"
+
+#include "HaltonScrambledSampler2D.h"
+
+#include <random>
+
+int main(int argc, char** argv)
+{
+	//generate a bunch of samples and write to an image file to show them.
+	size_t numSamples = 256;
+	FloatRange xrange(0, 1);
+	FloatRange yrange(0, 1);
+	//RNG stuff, pass into sampler
+	std::tr1::random_device rd;
+	std::tr1::mt19937 mt(rd());
+	//std::tr1::uniform_real_distribution<> distribution(0.0, 1.0);
+	
+	Sampler2D* sampler;
+	
+	//2D GRID
+
+	//unscrambled
+	sampler = new HaltonScrambledSampler2D(numSamples, &mt, xrange, yrange);
+	generateExampleImage2DSampleGrid(sampler, "HaltonScrambledSampler2D");
+	delete sampler;
+
+
+
+	//CHECKERS
+
+	numSamples = 4;
+
+	sampler = new HaltonScrambledSampler2D(numSamples, &mt, xrange, yrange);
+	generateExampleImageInfiniteCheckers(sampler, "HaltonScrambledSampler2D");
+	delete sampler;
+
+	return 0;
+}
